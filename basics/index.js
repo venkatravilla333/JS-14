9064a935d30a4a02a78794393d659a5313a74770
venkat ravilla <venkatravilla333@gmail.com>
2025-02-01 12:00:57 +0530

    object & array

--- basics/index.js
+++ basics/index.js
@@ -3,24 +3,79 @@
 //primitives
 
 //string
-console.log(a)
+// console.log(a)
 
-var a = "sachin"
+// var a = "sachin"
 
-console.log(a, typeof a)
+// console.log(a, typeof a)
 
-console.log(b)
-var b = 40
-console.log(b, typeof b)
+// console.log(b)
+// var b = 40
+// console.log(b, typeof b)
 
-console.log(c)
-var c = true
-console.log(c, typeof c)
+// console.log(c)
+// var c = true
+// console.log(c, typeof c)
 
-console.log(d)
-var d
-console.log(d, typeof d)
+// console.log(d)
+// var d
+// console.log(d, typeof d)
 
-console.log(e)
-var e = null
-console.log(e, typeof e)
+// console.log(e)
+// var e = null
+// console.log(e, typeof e)
+
+//reference
+
+// var person1 = {
+//   name1: 'sachin',
+//   age: 40,
+//   city: 'mumbai',
+//   play() {
+//     console.log('play')
+//   }
+// }
+
+// console.log(person1)
+// console.log(person1.name)
+// console.log(person1.age)
+// console.log(person1.city)
+// person1.play()
+
+// console.log(person1['name1'])
+// console.log(person1['age'])
+
+// var names = ['sachin', 'kohli', 'dhoni', 'gill']
+// var names = [ {
+//   name: 'sachin',
+//   age: 40,
+//   city: 'mumbai', 
+//   play: function () {
+//     console.log('play')
+//   }
+// }, {
+//   name: 'kohli',
+//   age: 30,
+//   city: 'delhi', 
+//   play: function () {
+//     console.log('play')
+//   }
+//   },
+//   {
+//   name: 'dhoni',
+//   age: 35,
+//   city: 'ranchi', 
+//   play: function () {
+//     console.log('play')
+//   }
+// }
+// ]
+
+
+// console.log(names)
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
+// console.log(names[3])
+
+
