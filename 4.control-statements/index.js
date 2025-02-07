4e64bc1aab4dd707c40092b93e2b39bbdf741d7c
venkat ravilla <venkatravilla333@gmail.com>
2025-02-07 11:25:26 +0530

    control statements

---
+++ 4.control-statements/index.js
@@ -0,0 +1,127 @@
+
+
+var marks = 20
+
+// if (marks < 35) {
+//   console.log('fail')
+// } else if (marks >= 35 && marks < 60) {
+//   console.log('second class')
+// } else if (marks >= 60 && marks < 75) {
+//   console.log('first class')
+// } else if (marks >= 75 && marks <= 100) {
+//   console.log('distinction')
+// } else {
+//   console.log('invalid marks')
+// }
+
+
+// switch (marks) {
+//   case marks < 35:
+//     console.log('fail')
+//     break;
+//   case marks >= 35 && marks < 60:
+//     console.log('second class')
+//     break;
+//   case marks >= 60 && marks < 75:
+//     console.log('first class')
+//     break;
+//   case marks >= 75 && marks <= 100:
+//     console.log('distinction')
+//     break;
+//   default:
+//     console.log('invalid marks')
+// }
+
+var weekDay = 1
+
+// if (weekDay === 1) {
+//   console.log('monday')
+// } else if (weekDay === 2) {
+//   console.log("thusday")
+// } else if (weekDay === 3) {
+//   console.log('wednesday')
+// } else if (weekDay === 4) {
+//   console.log('thusday')
+// } else if (weekDay === 5) {
+//   console.log('friday')
+// } else if (weekDay === 6) {
+//   console.log('saturday')
+// } else if (weekDay === 7) {
+//   console.log('sunday')
+// } else {
+//   console.log('invalid day')
+// }
+
+// switch (weekDay) {
+//   case 1:
+//     console.log('monday')
+//     break;
+//   case 2:
+//     console.log('thesday')
+//     break;
+//   case 3:
+//     console.log('wednesday')
+//     break;
+//   case 4:
+//     console.log('thusday')
+//     break;
+//   case 5:
+//     console.log('friday')
+//     break;
+//   case 6:
+//     console.log('saturday')
+//     break;
+//   case 7:
+//     console.log('sunday');
+//     break;
+//   default:
+//     console.log('invalid day')
+// }
+
+var customer = 'Gold'
+
+// if (customer === 'Gold') {
+//   console.log('gold customer')
+// } else if (customer === 'Silver') {
+//   console.log('silver customer')
+// } else if (customer === 'Iron') {
+//   console.log('iron customer')
+// } else {
+//   console.log('invalid customer')
+// }
+
+// switch (customer) {
+//   case 'Gold':
+//     console.log('gold customer')
+//     break;
+//   case 'Silver':
+//     console.log('silver customer')
+//     break;
+//   case 'Iron':
+//     console.log('iron customer')
+//     break;
+//   default:
+//     console.log('Invalid customer')
+// }
+
+// var enteredUserName = 'sachin'
+// var enteredPassword = '123456'
+
+// var dbUserName = 'sachin'
+// var dbPasword = '123456'
+
+// if (enteredUserName === dbUserName && enteredPassword === dbPasword) {
+//   console.log('Login success')
+// } else {
+//   console.log('invalid credentials')
+// }
+
+
+// switch (enteredUserName, enteredPassword) {
+//   case dbUserName && dbPasword:
+//     console.log('login success');
+//     break;
+//   default:
+//     console.log('invalid credentials')
+    
+// }
