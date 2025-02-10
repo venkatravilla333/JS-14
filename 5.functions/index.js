66879fec55145ad92299b185f360d86dd92ba13e
venkat ravilla <venkatravilla333@gmail.com>
2025-02-10 11:55:47 +0530

    functions

---
+++ 5.functions/index.js
@@ -0,0 +1,72 @@
+
+// //how many ways we can create function
+
+
+// var a = 100
+
+// //function declartion
+// function test() {
+//   var a = 10
+// }
+// test()
+
+// //function expression
+
+// var x = function test() {
+//   var b = 20
+// }
+// x()
+
+// var y = function () {
+//   var c = 30
+// }
+// y()
+
+// var z = () => {
+//   var d = 40
+// }
+// z()
+
+
+
+
+// var a = 1
+
+// function test() {
+  //   var a = 10
+  // }
+  // test()
+  
+  // var x = ()=> {
+    //   var b = 20
+    // }
+    
+    // x()
+    
+    
+//params vs arguments
+// var a = 100
+// var b  = 200
+// var c  = 300
+// function test(a, b, c) {
+//   var x = 10
+//   var y = 20
+//   var z = 30
+//   console.log(a)
+//   console.log(b)
+//   console.log(c)
+//   console.log(x)
+//   console.log(y)
+//   console.log(z)
+// }
+// test(1,2,3) //arguments
+
+// default parameter
+
+// function test(a=10) {
+  
+// }
+
+// test()
+
+
