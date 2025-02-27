7ea63990858e21c2eb3ff146472bc0b42a8f2fe6
venkat ravilla <venkatravilla333@gmail.com>
2025-02-27 10:56:52 +0530

    promises

--- 9.async-js/index.js
+++ 9.async-js/index.js
@@ -23,16 +23,87 @@
 // console.log('Bye')
 
 
-function getData(cb) {
-  setTimeout(() => {
-    var data = 'sachin'
-    cb(data)
-  }, 4000)
-}
-getData(function displayData(data) {
-  console.log(data)
-}
-)
+// function getData(num, cb) {
+//   setTimeout(() => {
+//     // var data = 'sachin'
+//     var data = num*num
+//     cb(data)
+//   }, 4000)
+// }
+// getData(2, (data1)=> {
+//   console.log(data1)
+//   getData(data1, (data2) => {
+//       console.log(data2)
+//     getData(data2, (data3) => {
+//         console.log(data3)
+//       getData(data3, (data4) => {
+//         console.log(data4)
+//           getData(data3, (data4) => {
+//            console.log(data4)
+//           })
+//         })
+//      })
+//   })
+//  }
+// )
 
 
+
+//Promises (ES-6)
+
+// function getData(num) {
+//   var prRes = new Promise((res, rej) => {
+//     setTimeout(() => {
+//       // var data = 'sachin'
+//       var data = num*num
+//       res(data)
+//      }, 4000)
+//   })
+//   return prRes
+// }
+
+
+// getData(2).then((data1) => {
+//   console.log(data1)
+//   return getData(data1)
+// }).then((data2) => {
+//   console.log(data2)
+//   return getData(data2)
+// }).then((data3) => {
+//   console.log(data3)
+//  return getData(data3)
+// }).then((data4) => {
+//   console.log(data4)
+// })
+
+//Async await (ES-6)
+
+// function getData(num) {
+//   var prRes = new Promise((res, rej) => {
+//     setTimeout(() => {
+//       // var data = 'sachin'
+//       var data = num*num
+//       res(data)
+//     }, 4000)
+//   })
+//   return prRes
+// }
+
+// getData()
+
+// async function displayData() {
+//   var data1 = await getData(2)
+//   console.log(data1)
+//   var data2 = await getData(data1)
+//   console.log(data2)
+//   var data3 = await getData(data2)
+//   console.log(data3)
+//   var data4 = await getData(data3)
+//   console.log(data4)
+// }
 // displayData()
+
+
+
+
+
