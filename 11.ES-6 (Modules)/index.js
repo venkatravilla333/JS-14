de7aca8a3e8c0b77e40fe5c68db19b75fbada919
venkat ravilla <venkatravilla333@gmail.com>
2025-03-02 11:38:40 +0530

    modules & webpack

---
+++ 11.ES-6 (Modules)/index.js
@@ -0,0 +1,17 @@
+
+import { y } from './one.js'
+import x from './one.js'
+
+import {arr} from './two.js'
+
+
+var a = 10
+
+var b = { name: 'sachin', age: 40 }
+
+console.log(a)
+console.log(b)
+console.log(x)
+console.log(y)
+console.log(arr)
+
---
+++ 11.ES-6 (Modules)/one.js
@@ -0,0 +1,10 @@
+
+import {arr} from './two.js'
+ var x = 100
+
+var y = { name: 'kohli', age: 30 }
+
+export default x
+// export default y
+console.log(arr)
+
